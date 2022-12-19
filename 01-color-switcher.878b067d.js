7c46e2c586b897fc6cfaf11fbeb45ce9dceadc7f
Stolbin <109080579+Stolbin@users.noreply.github.com>
2022-12-19 16:49:31 +0000

    Deploying to gh-pages from @ Stolbin/goit-js-hw-09@b33889fbc0ff0c38dc5c15facea2b282a1ea038f 🚀

---
+++ 01-color-switcher.878b067d.js
@@ -0,0 +1,2 @@
+!function(){var t={body:document.querySelector("body"),btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")},e=null,n=!1;t.btnStart.addEventListener("click",(function(){if(n)return;n=!0,t.btnStart.setAttribute("disabled",!0),e=setInterval((function(){return t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.btnStop.addEventListener("click",(function(){clearInterval(e),setTimeout((function(){location.reload()}),1e3)}))}();
+//# sourceMappingURL=01-color-switcher.878b067d.js.map
