b33889fbc0ff0c38dc5c15facea2b282a1ea038f
Stolbin <109080579+Stolbin@users.noreply.github.com>
2022-12-19 17:49:04 +0100

    upd

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
 
+let timerId = null;
+let isActive = false;
+
+refs.btnStart.addEventListener('click', startChangeColor);
+refs.btnStop.addEventListener('click', stopChangeColor);
+
+function startChangeColor() {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  refs.btnStart.setAttribute('disabled', true);
+  timerId = setInterval(
+    () => (refs.body.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+}
+
+function stopChangeColor() {
+  clearInterval(timerId);
+  setTimeout(function () {
+    location.reload();
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
